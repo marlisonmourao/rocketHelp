8e5043e9c224106256a01e20e1147f50fac31a9f
marlisonmourao <bentesmourao@gmail.com>
2023-04-15 19:56:21 -0400

    button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Button as NativeBaseButton, IButtonProps, Heading } from 'native-base'
+
+type Props = IButtonProps & {
+  title: string
+}
+
+export function Button({ title, ...rest }: Props) {
+  return (
+    <NativeBaseButton
+      bg="green.700"
+      h={14}
+      fontSize="sm"
+      rounded="sm"
+      _pressed={{ bg: 'gray.500' }}
+      {...rest}
+    >
+      <Heading color="white" fontSize="sm">
+        {title}
+      </Heading>
+    </NativeBaseButton>
+  )
+}

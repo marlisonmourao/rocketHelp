1544a4da0f95d991d08b6650c772d5cc13f82629
marlisonmourao <bentesmourao@gmail.com>
2023-04-16 18:05:02 -0400

    register new orders

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
+import auth from '@react-native-firebase/auth'
 import {
   Center,
   FlatList,
@@ -62,6 +64,15 @@ export function Home() {
     navigation.navigate('details', { orderId })
   }
 
+  function handleLogout() {
+    auth()
+      .signOut()
+      .catch((error) => {
+        console.log(error)
+        return Alert.alert('Não foi possível sair.')
+      })
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -75,7 +86,10 @@ export function Home() {
       >
         <Logo />
 
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+          onPress={handleLogout}
+        />
       </HStack>
 
       <VStack flex={1} px={6}>
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,3 +1,8 @@
+import { useState } from 'react'
+import { Alert } from 'react-native'
+import firestore from '@react-native-firebase/firestore'
+import { useNavigation } from '@react-navigation/native'
+
 import { VStack } from 'native-base'
 
 import { Header } from '@components/Header'
@@ -5,11 +10,47 @@ import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
 export function Register() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [patrimony, setPatrimony] = useState('')
+  const [description, setDescription] = useState('')
+
+  const navigation = useNavigation()
+
+  function handleNewOrderRegister() {
+    if (!patrimony || !description) {
+      return Alert.alert('Preencha todos os campos')
+    }
+
+    setIsLoading(true)
+
+    firestore()
+      .collection('orders')
+      .add({
+        patrimony,
+        description,
+        status: 'open',
+        created_at: firestore.FieldValue.serverTimestamp(),
+      })
+      .then(() => {
+        Alert.alert('Solicitação registrada com sucesso!')
+        navigation.goBack()
+      })
+      .catch((error) => {
+        console.log(error)
+        setIsLoading(false)
+        Alert.alert('Não foi possível registrar o pedido.')
+      })
+  }
+
   return (
     <VStack flex={1} p={6} bg="gray.600">
       <Header title="Nova solicitação" />
 
-      <Input placeholder="Número do património" mt={4} />
+      <Input
+        placeholder="Número do património"
+        mt={4}
+        onChangeText={setPatrimony}
+      />
 
       <Input
         placeholder="Descrição do problema"
@@ -17,9 +58,15 @@ export function Register() {
         flex={1}
         multiline
         textAlignVertical="top"
+        onChangeText={setDescription}
       />
 
-      <Button title="Cadatastrar" mt={5} />
+      <Button
+        title="Cadatastrar"
+        mt={5}
+        isLoading={isLoading}
+        onPress={handleNewOrderRegister}
+      />
     </VStack>
   )
 }
